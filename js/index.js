ea617771a7fd1c18dbd17e066b850cb17ca75c78
dayoun11 <4178ekdbs@naver.com>
2022-06-14 14:59:46 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".mo_view").css({"right": "-60%"}); /*처음위치*/
+	$(".close").hide();
+
+	$(".mo_menu_btn").click(function () {
+		$(".mo_view").stop(true, true).animate({"right": "0%"});
+		$(".close").show();
+	});
+
+	$(".close").click(function () {
+		$(".mo_view").stop(true, true).animate({"right": "-60%"});
+		$(".close").hide();
+	});
+});
